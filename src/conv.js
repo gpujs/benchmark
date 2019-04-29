0a7886b54d4d54d34b41a7c0f101964b6a4d9ba7
Harsh Khandeparkar <34770591+HarshKhandeparkar@users.noreply.github.com>
2019-04-29 19:45:23 +0000

    Update conv.js

--- src/conv.js
+++ src/conv.js
@@ -6,22 +6,21 @@ const kernel = [
 const kernelX = kernel[0].length,
   kernelY = kernel.length,
   paddingX = Math.floor(kernelX / 2),
-  paddingY = Math.floor(kernelY / 2)
+  paddingY = Math.floor(kernelY / 2);
 
 const paddificate = array => {
-  let out = []
+  let out = [];
 
     for (var y = 0; y < array.length + paddingY * 2; y++){
-      out.push([])
+      out.push([]);
       for (var x = 0; x < array[0].length + paddingX * 2; x++){
         const positionX = Math.min(Math.max(x - paddingX, 0), array[0].length - 1);
         const positionY = Math.min(Math.max(y - paddingY, 0), array.length - 1);
 
-        out[y].push(array[positionY][positionX])
+        out[y].push(array[positionY][positionX]);
       }
     }
-
-    return out
+    return out;
 }
 
 const matConvFunc = `function (array, kernel) {

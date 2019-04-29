dfa08c6a9437d14bf82da5f2c5738f3e826aa50c
Harsh <mjkgoa@gmail.com>
2019-04-30 00:12:14 +0530

    fix

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ console.log(`matrix generation time: ${benchmarks.matGen}ms`)
 console.log(`matrix multiplication compile time: ${benchmarks.build_time.matMult.gpu}ms`)
 console.log(`matrix multiplication (pipeline) compile time: ${benchmarks.build_time.matMult.pipe}ms`)
 
+console.log(``)
+
 console.log(`matrix multiplication run time: ${benchmarks.run_time.matMult.gpu}ms`)
 console.log(`matrix multiplication (pipeline) run time: ${benchmarks.run_time.matMult.pipe}ms`)

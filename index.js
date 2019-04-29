2f1c6a4a29eafd33ecdf90aecfc4eec171a9e59e
Harsh <mjkgoa@gmail.com>
2019-04-30 00:28:51 +0530

    change

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const bench = require('./src/index')
 const benchmarks = bench()
 
-console.log(`
-matrix generation time: ${benchmarks.matGen}ms`)
+console.log(``)
+console.log(`matrix generation time: ${benchmarks.matGen}ms`)
 
 console.log(`matrix multiplication compile time: ${benchmarks.build_time.matMult.gpu}ms`)
 console.log(`matrix multiplication (pipeline) compile time: ${benchmarks.build_time.matMult.pipe}ms`)
@@ -10,5 +10,14 @@ console.log(`matrix multiplication (pipeline) compile time: ${benchmarks.build_t
 console.log(``)
 
 console.log(`matrix multiplication run time: ${benchmarks.run_time.matMult.gpu}ms`)
-console.log(`matrix multiplication (pipeline) run time: ${benchmarks.run_time.matMult.pipe}ms
-`)
+console.log(`matrix multiplication (pipeline) run time: ${benchmarks.run_time.matMult.pipe}ms`)
+
+console.log(``)
+
+console.log(`matrix convolution compile time: ${benchmarks.build_time.matConv.gpu}ms`)
+console.log(`matrix convolution (pipeline) compile time: ${benchmarks.build_time.matConv.pipe}ms`)
+
+console.log(``)
+
+console.log(`matrix convolution run time: ${benchmarks.run_time.matConv.gpu}ms`)
+console.log(`matrix convolution (pipeline) run time: ${benchmarks.run_time.matConv.pipe}ms`)

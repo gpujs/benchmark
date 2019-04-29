ff15b94988c88cc9ab915b6d6c5cd73d52677228
Harsh Khandeparkar <34770591+HarshKhandeparkar@users.noreply.github.com>
2019-04-29 19:47:29 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const { GPU } = require('gpu.js'),
-  run = require('./run')
+  run = require('./run');
 
 const benchmark = (options = {}) => {
-  options.numBenchmarks = options.numBenchmarks || 1
+  options.numBenchmarks = options.numBenchmarks || 1;
 
-  options.matrixSize = options.matrixSize || 512
-  options.kernelSIze = options.kernelSIze || 3
+  options.matrixSize = options.matrixSize || 512;
+  options.kernelSIze = options.kernelSIze || 3;
 
-  options.gpu = options.gpu || new GPU({mode: 'gpu'})
-  options.cpu = options.cpu || new GPU({mode: 'cpu'})
+  options.gpu = options.gpu || new GPU({mode: 'gpu'});
+  options.cpu = options.cpu || new GPU({mode: 'cpu'});
 
-  return run(options)
+  return run(options);
 }
 
-module.exports = benchmark
+module.exports = benchmark;

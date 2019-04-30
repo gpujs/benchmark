ff19e98a0a9f7df3d4a7548599a8db929ed2e687
Harsh <mjkgoa@gmail.com>
2019-04-30 22:37:16 +0530

    no kernel size

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const benchmark = (options = {}) => {
 
   options.matrixSize = options.matrixSize || 512;
   options.output = [options.matrixSize, options.matrixSize];
-  options.kernelSIze = options.kernelSIze || 3;
 
   options.gpu = options.gpu || new GPU();
   options.cpu = options.cpu || new GPU({mode: 'cpu'});

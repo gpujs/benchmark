99e03a8721f44f005ee47a489aad016f2c840d0d
Harsh <mjkgoa@gmail.com>
2019-05-01 20:42:58 +0530

    fix diffs

--- src/stats/diff.js
+++ src/stats/diff.js
@@ -1,3 +1,3 @@
-const getDiff = (val1, val2) => Math.floor(((val1 - val2) / val2) * 10000) / 100;
+const getDiff = (val1, val2) => Math.floor(((val2 - val1) / val2) * 10000) / 100;
 
-module.exports = (val1, val2) => val1 > val2 ? {diff: getDiff(val1, val2), greater: 0} : {diff: getDiff(val2, val1), greater: 1};
+module.exports = (val1, val2) => val1 < val2 ? {diff: getDiff(val1, val2), greater: 0} : {diff: getDiff(val2, val1), greater: 1};

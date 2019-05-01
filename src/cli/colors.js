ca414bacc42c081e78d38018a90108e0559f7b65
Harsh <mjkgoa@gmail.com>
2019-05-01 22:37:26 +0530

    fix colors

--- src/cli/colors.js
+++ src/cli/colors.js
@@ -3,8 +3,8 @@ module.exports = {
   RED_NO_UNDER: '\033[1;31m', // red without underline
   GREEN_UNDER: '\033[0;32;4m', // underlined green
   GREEN_NO_UNDER: '\033[0;32m', // green without underline
-  YELLOW_UNDER: '\033[1;33;4m', // yellow underlined
-  YELLOW_NO_UNDER: '\033[1;33', // yello without underlined
+  YELLOW_UNDER: '\033[0;33;4m', // yellow underlined
+  YELLOW_NO_UNDER: '\033[0;33m', // yellow without underlined
   BG_WHITE: '\033[47;30;1m', // white background
   RED_FLASH: '\033[1;31;5m', // flashing/blinking red text
   NC: '\033[0m' // no color(default)

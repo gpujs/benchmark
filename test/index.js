98a8758fd60fa92977e573c2a338c9418184b3d0
Mr. X <mjkgoa@gmail.com>
2020-03-14 23:20:52 +0530

    improve defaults test.

--- test/index.js
+++ test/index.js
@@ -2,10 +2,7 @@ const test = require('tape'),
   index = require('../src/index').benchmark;
 
 test('index.js entry point test', t => {
-  const expectedDefaults = {
-      matrix_size: 512,
-      num_benchmarks: 1
-    },
+  const expectedDefaults = require('../src/util/defaults.json'),
     expectedNonDefaultOptions = {
       matrix_size: 1024,
       num_benchmarks: 5,
@@ -16,15 +13,13 @@ test('index.js entry point test', t => {
   const options = index().getDataField('options');
   const nonDefaultOptions = index(expectedNonDefaultOptions).getDataField('options');
 
-  t.equal(options.matrix_size, expectedDefaults.matrix_size, 'Correct matrix_size default');
-  t.equal(options.num_benchmarks, expectedDefaults.num_benchmarks, 'Correct num_benchmark default');
-  t.true(options.logs, 'Correct logs default');
-  t.true(options.cpu_benchmark, 'Correct cpu_benchmark default');
+  for (let def in expectedDefaults) {
+    t.equal(options[def], expectedDefaults[def], `Correct ${def} default.`);
+  }
 
-  t.equal(nonDefaultOptions.matrix_size, expectedNonDefaultOptions.matrix_size, 'Correct non-default matrix_size option loaded');
-  t.equal(nonDefaultOptions.num_benchmarks, expectedNonDefaultOptions.num_benchmarks, 'Correct non-default num_benchmarks option loaded');
-  t.true(!nonDefaultOptions.logs, 'Correct non-default logs option loaded');
-  t.true(!nonDefaultOptions.cpu_benchmark, 'Correct non-default cpu_benchmark option loaded');
+  for (let def in expectedNonDefaultOptions) {
+    t.equal(nonDefaultOptions[def], expectedNonDefaultOptions[def], `Correct ${def} non default.`);
+  }
   
   t.end();
 })

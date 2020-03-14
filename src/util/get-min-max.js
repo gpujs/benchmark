023428f52573c2af7a43e53ad658893eb06f43af
Mr. X <mjkgoa@gmail.com>
2020-03-14 23:01:17 +0530

    fix deviation

--- src/util/get-min-max.js
+++ src/util/get-min-max.js
@@ -5,12 +5,18 @@ module.exports = arr => {
       ) * 100
     ) / 100,
     min = arr.reduce((min, val) => Math.min(min, val)),
-    max = arr.reduce((max, val) => Math.max(max, val)),
-    deviation = Math.floor(
-      (
-        arr.reduce((difference, val) => difference + Math.abs(val - avg)) / avg
-      ) * 100
-    ) / 100;
+    max = arr.reduce((max, val) => Math.max(max, val));
+
+  // Deviation Calculation
+  let difference = 0;
+
+  arr.forEach(val => difference += Math.abs(val - avg));
+
+  const deviation = Math.floor(
+    (
+      difference / avg
+    ) * 100
+  ) / 100;
 
   return {
     avg,

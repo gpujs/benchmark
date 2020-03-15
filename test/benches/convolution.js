b9c95a0eed0a1645ed52604a109ce783e0232d37
Mr. X <mjkgoa@gmail.com>
2020-03-15 19:18:35 +0530

    improve convolution benchmark.

--- test/benches/convolution.js
+++ test/benches/convolution.js
@@ -1,5 +1,9 @@
 const test = require('tape'),
-  { kernelX, kernelY, paddingX, paddingY, paddificate } = require('../../src/benches/convolution');
+  { kernel, kernelX, kernelY, paddingX, paddingY, paddificate, generateFuncs } = require('../../src/benches/convolution'),
+  { GPU } = require('gpu.js');
+
+const gpu = new GPU({mode: 'gpu'});
+const cpu = new GPU({mode: 'cpu'});
 
 test('matrix convolution benchmark test', t => {
   const expectedKernelX = 3,
@@ -17,6 +21,11 @@ test('matrix convolution benchmark test', t => {
       [1, 1, 3, 4, 4],
       [5, 5, 5, 8, 8],
       [5, 5, 5, 8, 8]
+    ],
+    expectedConvolvedArray = [
+      [18, 23, 27],
+      [34, 42, 52],
+      [51, 65, 78]
     ]
   
   t.equal(kernelX, expectedKernelX, 'Correct kernel X width');
@@ -28,6 +37,15 @@ test('matrix convolution benchmark test', t => {
   const paddedArray = paddificate(array, paddingX, paddingY);
 
   t.deepEqual(paddedArray, expectedPaddedArray, 'paddificate returns correctly padded array');
+
+  const funcs = generateFuncs(gpu, cpu, [array.length, array.length]);
+
+  const gpuConvolve = funcs.gpu(expectedPaddedArray, kernel);
+    cpuConvolve = funcs.cpu(expectedPaddedArray, kernel);
+
+  t.deepEqual(cpuConvolve, gpuConvolve, 'CPU and GPU kernels return the correct value.');
+  t.deepEqual(gpuConvolve, expectedConvolvedArray, 'GPU kernel works correctly.');
+  t.deepEqual(cpuConvolve, expectedConvolvedArray, 'CPU kernel works correctly.');
 
   t.end();
 })

90c3504cccfc352ecfb572533cc6fbb41e4ffeba
Mr. X <mjkgoa@gmail.com>
2020-03-18 18:34:55 +0530

    CLI output bug fix

--- cli/log-min-max.js
+++ cli/log-min-max.js
@@ -1,6 +1,6 @@
 const { NC, YELLOW_UNDER } = require('./colors');
 
-const getVal = val => val == -1 ? 
+const getVal = val => val.avg == -1 ? 
   `${YELLOW_UNDER}Not Benchmarked${NC}` :
   `${YELLOW_UNDER}${val.avg}${NC} ms +- ${YELLOW_UNDER}${val.deviation}${NC}%`
 

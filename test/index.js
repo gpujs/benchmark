77fac1648bcb2a7b8e654f5e5e0d7d2ca16ef395
Harsh <mjkgoa@gmail.com>
2020-07-11 11:32:18 +0530

    test: fix for new changes

--- test/index.js
+++ test/index.js
@@ -1,16 +1,26 @@
-const test = require('tape'),
+const { GPU } = require('gpu.js'),
+  test = require('tape'),
   index = require('../src/index').benchmark;
+
+const gpu = new GPU(),
+  cpu = new GPU({ mode: 'cpu' });
 
 test('index.js entry point test', t => {
   const expectedDefaults = require('../src/util/defaults.json'),
     expectedNonDefaultOptions = {
+      gpu,
+      cpu,
       matrix_size: 1024,
       num_iterations: 5,
       logs: false,
       cpu_benchmark: false
     };
 
-  const options = index().getDataField('options');
+  const options = index({
+    gpu,
+    cpu,
+    gpu: new GPU(),
+  }).getDataField('options');
   const nonDefaultOptions = index(expectedNonDefaultOptions).getDataField('options');
 
   for (let def in expectedDefaults) {
